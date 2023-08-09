f22e1420d987be29e432f3c3c1a6506e1e657005
kaseypsbrice <kbrice@student.42adel.org.au>
2023-08-09 10:39:16 +0930

    Fixed cd issue

--- srcs/builtins.c
+++ srcs/builtins.c
@@ -6,7 +6,7 @@
 /*   By: kbrice <kbrice@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 08:34:44 by kbrice            #+#    #+#             */
-/*   Updated: 2023/08/09 09:00:14 by kbrice           ###   ########.fr       */
+/*   Updated: 2023/08/09 10:38:49 by kbrice           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,19 @@ int	ft_cd(char **argv)
 			perror("Home directory error");
 			return (1);
 		}
+		return (chdir(argv[1]));
 	}
 	else if (chdir(argv[1]) == -1)
 	{
 		ft_putstr_fd("cd : invalid path\n", STDERR_FILENO);
 		return (1);
 	}
-	return (chdir(argv[1]));
+	return (EXIT_FAILURE);
 }
 /* If there's no path, then the cd command will return to the home directory. 
  * Othewise, it'll change the directory to the specified path. 
  * If it fails to change directories, it'll produce an error message.
  */
-// [!] Suddenly Home is not set ??
 
 int	ft_pwd(char **argv)
 {
